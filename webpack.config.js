1f70daf77dcd1ed39762f589e6c2636604afc2b9
Lior Shefer <lior.shefer@wix.com>
2016-06-05 09:57:18 +0300

    support local setup for source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,51 @@
-var path = require('path')
-module.exports = {
-  entry: './index.js',
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+const TARGET = process.env.npm_lifecycle_event
+process.env.BABEL_ENV = TARGET
+
+const common = {
+  entry: {
+    app: './index.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     filename: 'bundle.js',
     publicPath: ''
   },
-  devtool: 'eval-source-map',
   externals: {
   },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react'},
-      {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader?functions=selector-parse&root=' + path.resolve('./js')}
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.js?$/,
+        loaders: ['babel-loader?presets[]=es2015&presets[]=react'],
+        exclude: /node_modules/
+      }
     ]
   }
+}
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map'
+  })
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {})
 }
